7af2951dad8b37a6253dce65ce5c9970ebe1788f
Shreya Vinodh <shreyavinod09@gmail.com>
2021-01-23 18:28:53 -0500

    basic nodejs server

--- nodejsServer.js
+++ nodejsServer.js
@@ -5,5 +5,7 @@ var server = http.createServer(function(req, res){
     res.end('Hello World');
 });
 
-server.listen(3000, '127.0.0.1');    //listen at local IP address at port 3000
-console.log('listening at port 3000');
+server.listen(3000, '127.0.0.1', () => {        //listen at local IP address at port 3000
+    console.log('listening at port 3000');
+});  
+
